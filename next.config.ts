5b84357dfed1219c23563643995068d45e16842d
trungpham71198 <phamdinhtrung7111998@gmail.com>
2024-12-05 00:22:18 +0700

    :tada: feat(init): init

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+export default
+bundleAnalyzer(
+  {
+    eslint: {
+      dirs: ['.'],
+    },
+    poweredByHeader: false,
+    reactStrictMode: true,
+  },
+);
